58ebbd475c8cad4dcd6ebeea1913137200cabc42
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-05-22 21:54:27 +0100

    new change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { MobileHeader } from './Components/Header/MobileHeader';
 
 function App() {
   return (
     <div className="App">
-
+  <MobileHeader/>
     </div>
   );
 }
---
+++ src/Components/Header/MobileHeader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {FaBars} from 'react-icons/fa';
+import  {HiOutlineXMark} from 'react-icons/hi';
+
+export const MobileHeader = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
