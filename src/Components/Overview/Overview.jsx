bd7e5f20e95065f3bbd7f2d0238727ad35ccc4ca
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-05-23 15:19:24 +0100

    Overview

---
+++ src/Components/Overview/Overview.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import oilTools from '../../Images/bg4.avif';
+import { Link } from "react-router-dom";
+
+export const Overview = () => {
+    return (
+        <div className="px-[20px] md:min-h-[70vh] lg:min-h-[100vh] items-center py-[50px] justify-around  bg-slate-900 md:flex-row-reverse  flex flex-col gap-5 md:flex-row">
+
+<div className="max-w-[600px] flex flex-col gap-4">
+    <h2 className="text-white font-semibold text-[20px] ">
+    "We are Lorddube logistic energy.co, your trusted partner for top-of-the-line oil and gas tools. With our unwavering commitment to excellence and a comprehensive range of industry-leading products, we empower businesses in the oil and gas sector to achieve unmatched performance and operational efficiency "
+    </h2>
+    <p className="text-slate-200 font-fonty ">At ExcellOil Tools, we understand the critical role that reliable tools play in your operations. That's why we go above and beyond to deliver products of exceptional quality and reliability. Our tools are manufactured using cutting-edge techniques and premium materials, ensuring they can withstand the most challenging environments and deliver consistent performance under demanding conditions.</p>
+    <Link className="bg-sky-500 shadow w-fit p-2 text-slate-50 text-[25px] rounded font-myfont">View Products</Link>
+</div>
+<div className="max-w-[500px] ">
+    <img src={oilTools} alt="" className="w-full h-full  rounded" />
+</div>
+        </div>
+    )
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { HeroSection } from "../../Components/Hero Section/HeroSection";
-
+import { Overview } from "../../Components/Overview/Overview";
 
 export const Home = () => {
     return(
         <div>
 <HeroSection/>
+<Overview/>
         </div>
     )
 }
