ef3be399794c658113b6ef147aab3dc7e7768b58
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-05-23 14:44:41 +0100

    new change

--- src/Components/Hero Section/HeroSection.jsx
+++ src/Components/Hero Section/HeroSection.jsx
@@ -24,7 +24,7 @@ export const HeroSection = () => {
         <div>
       <div 
         style={{ backgroundImage: `url(${HeroArray[currentImage]})` }}
-      className={`min-h-[100vh] relative after:left-0 after:right-0 after:absolute after:top-0 after:h-full flex items-center after:w-full after:bg-Icon pt-[50px]  px-[30px] bg-cover z-[1]  `}>
+      className={`min-h-[100vh] relative after:left-0 after:right-0 after:absolute after:top-0 after:h-full flex items-center after:w-full after:bg-Icon  px-[30px] bg-cover z-[1]  `}>
         <div className="max-w-[700px] relative z-[5] flex flex-col gap-3 ">
         <h1 className="text-white relative uppercase font-bold text-[30px] md:text-[50px] z-[2] ">
         Lorddube logistic energy.co
@@ -32,7 +32,7 @@ export const HeroSection = () => {
        <h2></h2>
        <p className="text-slate-100 wrap text-[20px] font-fonty md:text-[25px] ">Welcome to <strong className="text-white font-bold capitalize">Lorddube logistic energy.co</strong>, your trusted Your Reliable Source for High-Quality Oil and Gas Tools.</p>
        <div className="my-3 flex flex-row gap-5">
-<Link className="bg-slate-50 p-2 hover:bg-yellow-500 text-[20px] font-bold text-slate-900 rounded shadow-2xl ">Contact Us</Link> 
+<Link className="bg-slate-50 p-2 hover:bg-yellow-500 text-[20px] font-myfont text-slate-900 rounded shadow-2xl ">Contact Us</Link> 
 <Link className="border hover:bg-black p-2 text-[20px] font-myfont text-slate-50 rounded shadow-2xl ">Read More</Link> 
        </div>
         </div>
