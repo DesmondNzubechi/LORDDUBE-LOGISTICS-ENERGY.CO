e6afa287bd92f9c692bea477cf8f355cc67ff750
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-05-23 03:39:26 +0100

    mobile navlink

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import { MobileHeader } from './Components/Header/MobileHeader';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+ <div className="App">
   <MobileHeader/>
+  <Routes>
+    
+  </Routes>
     </div>
+    </BrowserRouter>
+   
   );
 }
 
--- src/Components/Header/MobileHeader.jsx
+++ src/Components/Header/MobileHeader.jsx
@@ -1,11 +1,35 @@
 import React from "react";
-import {FaBars} from 'react-icons/fa';
-import  {HiOutlineXMark} from 'react-icons/hi';
+import { NavLink } from "react-router-dom";
+
+export const Navlink = [
+    {link: '/', linkName: 'Home'},
+    {link: '/service', linkName: 'Services'},
+    {link: '/about', linkName: 'About'},
+    {link: '/products', linkName: 'Product'},
+    {link: '/contact', linkName: 'Contact'},
+]
 
 export const MobileHeader = () => {
-    return(
+  return(
+    <div>
         <div>
-
+            <div><h1>lorddube</h1></div>
         </div>
-    )
+        {
+            Navlink.map(links => {
+                return(
+                    <ul>
+                        <li>
+                            <NavLink to={links.link} style={({isActive}) => {
+return isActive ? {borderBottom : '5px solid white'} : {}
+}} className="xl:text-[17px] text-[14px] " >
+                             {links.linkName}
+                            </NavLink>
+                        </li>
+                    </ul>
+                )
+            })
+        }
+    </div>
+  )
 }
