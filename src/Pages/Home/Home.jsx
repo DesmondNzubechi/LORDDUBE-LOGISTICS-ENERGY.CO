29c90447753386302910a01ee21ab9195094a832
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-05-23 16:45:35 +0100

    new change

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { HeroSection } from "../../Components/Hero Section/HeroSection";
 import { Overview } from "../../Components/Overview/Overview";
-
+import { AboutHome } from "../../Components/About/About";
 export const Home = () => {
     return(
-        <div>
+        <div className="bg-slate-900">
 <HeroSection/>
 <Overview/>
+<AboutHome/>
         </div>
     )
 }
